1308dbbcc8a8930e8418d42c88e620bd883fded9
unknown <enricsd999@gmail.com>
2026-08-10 17:23:19 +0800

    text align changes

--- App.jsx
+++ App.jsx
@@ -5,12 +5,7 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import {  StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
