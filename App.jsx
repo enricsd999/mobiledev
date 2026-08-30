388d6a3fbe224c62367b5c4915d09a982ff4b785
unknown <enricsd999@gmail.com>
2026-08-30 21:06:45 +0800

    first

--- App.jsx
+++ App.jsx
@@ -20,29 +20,12 @@ export default function App() {
             height: 200,
           }}
         />
-        <View style={styles.column}>
-          <View style={styles.row}>
-            <Text style={styles.profileLabel}>Nama</Text>
-            <Text style={styles.profileColon}>:</Text>
-            <Text style={styles.profileText}>Enrico</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.profileLabel}>Fakultas</Text>
-            <Text style={styles.profileColon}>:</Text>
-            <Text style={styles.profileText}>Ilmu Komputer</Text>
-          </View>
-        </View>
-        {/* <Text style={styles.profileText}>Nama: Enrico</Text>
+        <Text style={styles.profileText}>Nama: Enrico</Text>
         <Text style={styles.profileText}>Fakultas: Ilmu Komputer</Text>
-        <Text style={styles.profileText}>Program Studi: Informatika</Text> */}
+        <Text style={styles.profileText}>Program Studi: Informatika</Text>
       </View>
       <View style={styles.footer}>
-        {/* <View style={styles.footerSub}> */}
-        <View style={styles.footerItems}>
           <Text>@Copyright</Text>
-        </View>
-        {/* </View> */}
-        {/* <Text>Ini Footer saya</Text> */}
       </View>
     </View>
   );
@@ -86,33 +69,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
-  profileLabel: {
-    marginTop: 15,
-    fontSize: 24,
-    // flex: 1,
-    width: 120,
-    // textAlign: 'center',
-  },
   profileText: {
     marginTop: 15,
     fontSize: 24,
-    flex: 1,
-    // textAlign: 'center',
-  },
-  profileColon: {
-    marginTop: 15,
-    fontSize: 24,
-    width: 30,
     textAlign: 'center',
-  },
-  column: {
-    flex: 0.5,
-    flexDirection: 'column',
-  },
-  row: {
-    width: 400,
-    flexDirection: 'row',
-    justifyContent: 'center',
   },
 });
 
